925ac1e0598ab98db5a48c9ec404811ecbd0e3db
ummul fatima <ummulf02@gmail.com>
2025-10-20 14:58:26 -0700

    Add files via upload

---
+++ templetApp/controller/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace templetApp.controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewData work as a dictionary KEY VALUE Pair
+            ViewData["name"] = "Hello From Testing ViewData";
+            ViewData["date"] = DateTime.Now;
+
+            string[] fruits = { "Apple", "Mango", "Banan", "Orange", "Grapes" };
+            ViewData["fruitList"] = fruits;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //ViewBag work as a Class
+            ViewBag.details = "This is the intro Application.";
+            string[] students = { "Faizan", "Ali", "Yawar" };
+            ViewBag.stds = students;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            TempData["city"] = "Karachi";
+            TempData["phone"] = "021-33448894";
+                return View();
+        }
+
+        public IActionResult FormSubmit()
+        {
+            TempData["status"] = true;
+            return RedirectToAction("Students");
+        }
+
+        public IActionResult Students()
+        {
+            return View();
+        }
+    }
+}
