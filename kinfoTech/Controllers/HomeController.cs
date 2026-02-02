c463277fd1d4bab40d28c6f6432a763407b61e40
ummul fatima <ummulf02@gmail.com>
2026-02-02 11:28:47 -0800

    Add files via upload

---
+++ kinfoTech/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kinfoTech.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
